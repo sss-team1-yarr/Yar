77b44b254d1af1a99904147dc48f1d3fd9a71db3
reverrie37 <reverrie37@gmail.com>
2026-03-30 08:48:52 +0900

    I don't know

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -11,7 +11,7 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         Debug.Log("update");
     }
