6c0d8e75efa2355a581539224aaefa9c58a31275
kealean <uiseunghan62@gmail.com>
2026-03-30 08:46:47 +0900

    wow this is great

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -3,10 +3,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private Rigidbody2D rb;
-
-    private void Awake() {
-        rb = GetComponent<Rigidbody2D>();
+    private void Update()
+    {
+        Debug.Log("update");
     }
 }
-    
