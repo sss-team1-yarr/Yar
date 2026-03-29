b76a25c65a68a449a8c5ccb1f63702fbb9a4dd20
kealean <uiseunghan62@gmail.com>
2026-03-30 08:47:57 +0900

    rb readd

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,8 +1,16 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     private void Update()
     {
         Debug.Log("update");
