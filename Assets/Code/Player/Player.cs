0a7971a46e748a805d48ac2b3e775ae3e7271874
kealean <uiseunghan62@gmail.com>
2026-03-30 09:34:52 +0900

    sorry

---
+++ Assets/Code/Player/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Code.Player
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        [SerializeField] private Rigidbody2D rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            Debug.Log("update");
+            rb.linearVelocity = rb.velocity;
+        }
+    }
+}
