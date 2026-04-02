a3fa442ebe5841c986af20c558e2202fb9924899
kealean <uiseunghan62@gmail.com>
2026-04-02 19:23:50 +0900

    Made InputReceiver.cs OnMove and OnJump

--- Assets/Code/Player/Components/InputReceiver.cs
+++ Assets/Code/Player/Components/InputReceiver.cs
@@ -1,13 +1,35 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
+
+public delegate void OnJump();
+public delegate void OnMove();
 
 namespace Code.Player.Components
 {
     public class InputReceiver : MonoBehaviour, Controls.IPlayerActions
     {
+        private Controls _controls;
+        public event Action OnJumpInput;
+        public event Action<float> OnMoveInput;
+
+        private void Awake()
+        {
+            _controls = new Controls();
+            _controls.Player.Enable();
+            _controls.Player.SetCallbacks(this);
+        }
+
+        private void OnDestroy()
+        {
+            _controls.Disable();
+            _controls.Dispose();
+            _controls = null;
+        }
+        
         public void OnMove(InputAction.CallbackContext context)
         {
-            
+            OnMoveInput?.Invoke(context.ReadValue<Vector2>().x);
         }
 
         public void OnAttack(InputAction.CallbackContext context)
@@ -17,7 +39,8 @@ namespace Code.Player.Components
 
         public void OnJump(InputAction.CallbackContext context)
         {
-            
+            if (context.performed)
+                OnJumpInput?.Invoke();
         }
 
         public void OnRun(InputAction.CallbackContext context)
