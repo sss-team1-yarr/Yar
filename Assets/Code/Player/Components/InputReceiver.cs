cadaa1ae100a41bb9702e69207ae82add097da23
kealean <uiseunghan62@gmail.com>
2026-04-02 19:17:22 +0900

    controls update

--- Assets/Code/Player/Components/InputReceiver.cs
+++ Assets/Code/Player/Components/InputReceiver.cs
@@ -10,22 +10,7 @@ namespace Code.Player.Components
             throw new System.NotImplementedException();
         }
 
-        public void OnLook(InputAction.CallbackContext context)
-        {
-            throw new System.NotImplementedException();
-        }
-
         public void OnAttack(InputAction.CallbackContext context)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void OnInteract(InputAction.CallbackContext context)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void OnCrouch(InputAction.CallbackContext context)
         {
             throw new System.NotImplementedException();
         }
@@ -35,17 +20,7 @@ namespace Code.Player.Components
             throw new System.NotImplementedException();
         }
 
-        public void OnPrevious(InputAction.CallbackContext context)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void OnNext(InputAction.CallbackContext context)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void OnSprint(InputAction.CallbackContext context)
+        public void OnRun(InputAction.CallbackContext context)
         {
             throw new System.NotImplementedException();
         }
