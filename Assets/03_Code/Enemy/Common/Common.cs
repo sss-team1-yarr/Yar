c5a9542b2c4e79672ddfed553ff599af22c2c230
reverrie37 <reverrie37@gmail.com>
2026-04-08 20:17:17 +0900

    i'm genius

--- Assets/03_Code/Enemy/Common/Common.cs
+++ Assets/03_Code/Enemy/Common/Common.cs
@@ -1,9 +1,38 @@
-﻿using UnityEngine;
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 namespace _03_Code.Enemy.Common
 {
     public class Common : MonoBehaviour
     {
-        
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private float moveSpeed = 2f;
+        [SerializeField] private Transform playerTrm;
+
+        private Vector2 moveDir;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            playerTrm = GameObject.Find("Player").transform;
+            moveDir = playerTrm.position - transform.position;
+            moveDir.Normalize(); 
+        }
+
+        private void FixedUpdate()
+        {
+            rb.linearVelocityX = moveDir.x * moveSpeed; 
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            //Destroy(collision.gameObject);
+        }
     }
 }
