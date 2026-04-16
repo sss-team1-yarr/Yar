71197e68523417e4379569fcd97eb4b9ade892fa
reverrie37 <reverrie37@gmail.com>
2026-04-16 18:50:59 +0900

    make UI restart and exit

--- Assets/03_Code/UI/Pause/PauseUI.cs
+++ Assets/03_Code/UI/Pause/PauseUI.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace _03_Code.UI.Pause {
     public class PauseUI : MonoBehaviour {
-        
+        public void Restart()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void Exit()
+        {
+            Application.Quit();
+        }
     }
 }
