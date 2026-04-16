a8952a4f25bb00bcdf66221473d5cc8d897accb5
reverrie37 <reverrie37@gmail.com>
2026-04-16 19:00:41 +0900

    Update GameScene UI

--- Assets/03_Code/UI/Pause/PauseUI.cs
+++ Assets/03_Code/UI/Pause/PauseUI.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 namespace _03_Code.UI.Pause {
     public class PauseUI : MonoBehaviour {
-        public void Restart()
+
+        private void Start()
         {
-            SceneManager.LoadScene(1);
+            gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (Keyboard.current.escapeKey.wasPressedThisFrame)
+            {
+                gameObject.SetActive(true);
+            }
+        }
+
+        public void ReturnGame()
+        {
+            gameObject.SetActive(true);
         }
 
         public void Exit()
         {
-            Application.Quit();
+            SceneManager.LoadScene(0);
         }
     }
 }
