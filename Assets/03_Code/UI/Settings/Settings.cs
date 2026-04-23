96c9a5d43d4ad97d52b90b5f10eefa16ae865014
kealean <uiseunghan62@gmail.com>
2026-04-23 20:24:49 +0900

    many... change and make settings

--- Assets/03_Code/UI/Settings/Settings.cs
+++ Assets/03_Code/UI/Settings/Settings.cs
@@ -1,7 +1,34 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace _03_Code.UI.Settings {
     public class Settings : MonoBehaviour {
+        [SerializeField] private GameObject pauseUI;
+        [SerializeField] private Player.Main.Player player;
         
+        private void Start() {
+            gameObject.SetActive(false);
+        }
+
+        private void Update() {
+            if (gameObject.activeSelf) {
+                if (Keyboard.current.escapeKey.wasPressedThisFrame) {
+                    player.isActived = true;
+                    pauseUI.SetActive(true);
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+        
+        public void BackToMenu() {
+            player.isActived = !player.isActived;
+            gameObject.SetActive(false);
+            pauseUI.SetActive(true);
+        }
+        
+        private void OnEnable() {
+            Time.timeScale = 0f;
+        }
     }
 }
