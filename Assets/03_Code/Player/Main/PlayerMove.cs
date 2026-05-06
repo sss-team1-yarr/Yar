74452926c887f37131a4f90494f765c25c31dab5
reverrie37 <reverrie37@gmail.com>
2026-05-06 20:13:33 +0900

    and some more..

--- Assets/03_Code/Player/Main/PlayerMove.cs
+++ Assets/03_Code/Player/Main/PlayerMove.cs
@@ -1,131 +1,19 @@
-using System.Collections;
-using _03_Code.Items;
-using _03_Code.Items.Weapons;
-using _03_Code.Player.Components;
-using _03_Code.Player.Input;
-using _03_Code.Player.Interface;
-using Unity.Cinemachine;
 using UnityEngine;
 
-namespace _03_Code.Player.Main {
-    public class PlayerMove : MonoBehaviour, IPlayerModule {
-        [SerializeField] private int explosion;
-        [SerializeField] private float jumpForce;
-        [SerializeField] private AnimationControl ani;
-        [SerializeField] private HpManager hp;
-        [SerializeField] private ParticleSystem vfx;
-        [SerializeField] private GameObject vfxBoom;
-        [SerializeField] private ContactChecker contactChecker;
-        [SerializeField] private CinemachineImpulseSource impulseSource;
-
-        [field: SerializeField] public Weapon HoldingItem { get; private set; }
-        //private TestDamageable[] testDam;
-
-        private Player _owner;
+namespace _03_Code.Player.Main
+{
+    public class PlayerMove : MonoBehaviour
+    {
         private Rigidbody2D _rb;
-        private InputReceiver _input;
 
-        public bool RotationRight { get; private set; }
-
-        private bool _isDashing;
-        [SerializeField] private float dashForce = 20f;
-        [SerializeField] private float dashDuration = 0.2f;
-        [SerializeField] private ParticleSystem dashVfx;
-
-        public void Initialize(Player owner) {
-            _owner = owner;
-            _rb = owner.GetComponent<Rigidbody2D>();
-            _input = _owner.GetModule<InputReceiver>();
-            _owner.GetModule<AnimationControl>();
-            _input.OnJumpInput += HandleJump;
-            _input.OnRunInput += HandleRun;
-            _input.OnMoveInput += HandleMoveInput;
-            _input.OnAttackInput += HandleAttackInput;
-            _input.OnSkill1Input += HandleSkill1Input;
-            _input.OnSkill2Input += HandleSkill2Input;
-            _input.OnGuardInput += HandleGuard;
+        private void Awake()
+        {
+            _rb = GetComponentInParent<Rigidbody2D>();
         }
-
-        private void HandleGuard() {
-            //what is this?
-            Destroy(gameObject);
-            Time.timeScale = 0.2f;
-        }
-
-        private void Awake() {
-            HoldingItem?.HoldItem(new ItemUsingContext { User = _owner, Input = 0, Pressed = true });
-        }
-
-        private float Speed { get; set; } = 8f;
-        public float MoveInput { get; private set; }
-
-        private void HandleMoveInput(float value) {
-            MoveInput = value;
-            ani.OnMoveAni(Mathf.Abs(value));
-            if (!Mathf.Approximately(value, 0f)) {
-                RotationRight = value > 0f;
-                _owner.transform.rotation = Quaternion.Euler(0f, RotationRight ? 0f : 180f, 0f);
-            }
-        }
-
-        private void HandleRun(bool run) {
-            Speed *= run ? 2 : 0.5f;
-        }
-
-        private void HandleJump() {
-            const float multiplier = 3f;
-            if (contactChecker.IsGrounded)
-            {
-                _rb.linearVelocity = Vector2.zero;
-                _rb.AddForceY(jumpForce * multiplier, ForceMode2D.Impulse);
-            }
-        }
-
-        private void HandleAttackInput(int btn, bool pressed) {
-            HoldingItem?.Use(new ItemUsingContext {
-                Input = btn,
-                Pressed = pressed,
-                User = _owner
-            });
-        }
-
-        private void HandleSkill1Input() {
-            hp.UpdateHp(explosion);
-            vfxBoom.transform.position = transform.position;
-            impulseSource.GenerateImpulseWithForce(1f);
-            //for (var i = 0; i < testDam.Length; i++) testDam[i]?.largePush();
-            vfx.Play();
-        }
-
-        private void HandleSkill2Input() {
-            if (_isDashing) return;
-            StartCoroutine(Dash());
-            dashVfx?.Play();
-        }
-
+        
         private void FixedUpdate() {
-            if (_isDashing) return;
-            _rb.linearVelocityX = MoveInput * Speed;
-        }
-
-        private IEnumerator Dash() {
-            _isDashing = true;
-            ani.OnDashAni(true);
-            var dashDirection = RotationRight ? 2f : -2f;
-            _rb.linearVelocity = new Vector2(dashDirection * dashForce, 0f);
-
-            yield return new WaitForSeconds(dashDuration);
-            ani.OnDashAni(false);
-            _isDashing = false;
-        }
-
-        private void OnDestroy() {
-            _input.OnMoveInput -= HandleMoveInput;
-            _input.OnRunInput -= HandleRun;
-            _input.OnJumpInput -= HandleJump;
-            _input.OnAttackInput -= HandleAttackInput;
-            _input.OnSkill1Input -= HandleSkill1Input;
-            _input.OnSkill2Input -= HandleSkill2Input;
+            if (GameManager.Instance.playerSystem.IsDashing || GameManager.Instance.playerHit.IsApproach) return;
+            _rb.linearVelocityX = GameManager.Instance.playerSystem.MoveInput * GameManager.Instance.playerSystem.Speed;
         }
     }
 }
