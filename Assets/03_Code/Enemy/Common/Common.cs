1cca63ad78673d041c98a2a7aa92c4c7d8ea3326
reverrie37 <reverrie37@gmail.com>
2026-05-07 18:49:36 +0900

    fix common script

--- Assets/03_Code/Enemy/Common/Common.cs
+++ Assets/03_Code/Enemy/Common/Common.cs
@@ -10,10 +10,9 @@ namespace _03_Code.Enemy.Common {
         [SerializeField] private int damage;
         [SerializeField] private HpManager hp;
         [SerializeField] private PlayerRenderer playerRenderer;
-        [SerializeField] private IsNearbyMe rc;
         [SerializeField] private float speed = 2f;
         [SerializeField] private float enemyKnockBackForce = 5f;
-        [SerializeField] private float hitDuration = 0.5f;
+        [SerializeField] private float hitDuration = 0.1f;
 
         private IsNearbyMe _scan;
         private bool _isHit;
@@ -23,7 +22,7 @@ namespace _03_Code.Enemy.Common {
         }
 
         private void Update() {
-            transform.rotation = Quaternion.Euler(0f, rc.MoveDir.x > 0f ? 0f : 180f, 0f);
+            transform.rotation = Quaternion.Euler(0f, _scan.MoveDir.x > 0f ? 0f : 180f, 0f);
         }
 
         private void OnTriggerEnter2D(Collider2D other) {
@@ -31,14 +30,14 @@ namespace _03_Code.Enemy.Common {
         }
 
         private void FixedUpdate() {
-            if(_isHit)
+            if(!_isHit)
                 rb.linearVelocity = _scan.MoveDir.normalized * speed;
         }
 
         public IEnumerator Hit(float knockbackForce) {
             if (_isHit) yield break; 
             _isHit = true;
-            float dir = transform.position.x > GameManager.Instance.playerSystem.transform.position.x ? 1f : -1f;
+            float dir = transform.position.x > GameManager.Instance.playerControl.transform.position.x ? 1f : -1f;
             
             rb.linearVelocity = Vector2.zero;
             rb.AddForce(new Vector2(knockbackForce * dir * enemyKnockBackForce, knockbackForce * enemyKnockBackForce), 
