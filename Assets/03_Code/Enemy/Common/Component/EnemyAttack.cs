e49761b84f4557085bff4c9752ec246b003c9a20
reverrie37 <reverrie37@gmail.com>
2026-05-13 17:32:42 +0900

    Make EnemySO System

--- Assets/03_Code/Enemy/Common/Component/EnemyAttack.cs
+++ Assets/03_Code/Enemy/Common/Component/EnemyAttack.cs
@@ -22,7 +22,7 @@ namespace _03_Code.Enemy.Common.Component
             {
                 Vector2 direction = - collision.contacts[0].normal;
                 Debug.Log(direction);
-                StartCoroutine(GameManager.Instance.playerHit.Approach(direction));
+                StartCoroutine(GameManager.Instance.playerHit.Approach(direction, em));
             }
         }
     }
