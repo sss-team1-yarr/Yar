1acf2039f4d26d486bc6a8ecc14604d8148ae8e4
reverrie37 <reverrie37@gmail.com>
2026-05-13 18:51:13 +0900

    remove Debug.Log

--- Assets/03_Code/Enemy/Common/Component/EnemyAttack.cs
+++ Assets/03_Code/Enemy/Common/Component/EnemyAttack.cs
@@ -21,7 +21,6 @@ namespace _03_Code.Enemy.Common.Component
             if (collision.gameObject.CompareTag("Player"))
             {
                 Vector2 direction = - collision.contacts[0].normal;
-                Debug.Log(direction);
                 StartCoroutine(GameManager.Instance.playerHit.Approach(direction, em));
             }
         }
