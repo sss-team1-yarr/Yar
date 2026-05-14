82fd2b4010235da726e99960a6c337d4f0f50fc8
reverrie37 <reverrie37@gmail.com>
2026-05-14 19:48:10 +0900

    make example

--- Assets/03_Code/Enemy/Common/Component/IgnoreEnemyScale.cs
+++ Assets/03_Code/Enemy/Common/Component/IgnoreEnemyScale.cs
@@ -5,7 +5,7 @@ namespace _03_Code.Enemy.Common.Component
 {
     public class IgnoreEnemyScale : MonoBehaviour
     {
-        private void Awake()
+        private void Start()
         {
             Vector3 parentScale = transform.parent.lossyScale;
             transform.localScale = new Vector3(1f / parentScale.x, 1f / parentScale.y, transform.localScale.z);
