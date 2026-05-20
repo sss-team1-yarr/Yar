7849ba5f0e0f716a4baec10abd343da9d177b143
siwoopark10-hlm <eden2010.park@gmail.com>
2026-05-20 17:48:16 +0900

    I make boss Hand Attack and fix base key

---
+++ Assets/03_Code/Enemy/Boss/Skill/HandAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using _03_Code.Player.Components;
+using UnityEngine;
+
+namespace _03_Code.Enemy.Boss.Skill {
+    public class HandAttack : MonoBehaviour {
+        [SerializeField] private Rigidbody2D rb;
+        
+
+        private readonly WaitForSeconds _skillSeconds = new WaitForSeconds(2f);
+
+        private void OnEnable() {
+            StartCoroutine(HandLifeCoroutine());
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.CompareTag("Player")) HpManager.Instance.Damage(10);
+            
+        }
+
+        private IEnumerator HandLifeCoroutine() {
+            yield return _skillSeconds;
+            Destroy(gameObject);
+        }
+    }
+}
